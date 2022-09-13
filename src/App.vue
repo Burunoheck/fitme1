d4b1aba450278f482596b6cd76b23303992cc1c7
Burunoheck <brunoreeck6@gmail.com>
2022-09-12 23:56:50 -0300

    fitme primeiro commit

---
+++ src/App.vue
@@ -0,0 +1,87 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary" dark elevation="3">
+      <v-app-bar-nav-icon @click.stop="sidebar = !sidebar"></v-app-bar-nav-icon>
+        <v-list-item>
+          <h3>Fitme</h3>
+        </v-list-item>
+      <v-spacer></v-spacer>
+      <v-icon>mdi-account</v-icon>
+    </v-app-bar>
+    <v-navigation-drawer app v-model="sidebar" :mini-variant.sync="mini">
+      <v-list dense color="primary" dark>
+        <v-list-item>
+          <v-list-item-action>
+            <v-icon @click.stop="sidebar = !sidebar">
+              mdi-chevron-left
+            </v-icon>
+          </v-list-item-action>
+        </v-list-item>
+      </v-list>
+      <v-list-item class="px=2" @click="mini = !mini">
+        <v-list-item-avatar>
+          <v-icon>
+            mdi-account
+          </v-icon>
+        </v-list-item-avatar>
+        <v-list-item-content>
+          Lucas Reeck de Eusebio
+        </v-list-item-content>
+        <v-btn icon small>
+          <v-icon>
+            mdi-chevron-left
+          </v-icon>
+        </v-btn>
+      </v-list-item>
+      <v-divider>
+
+      </v-divider>
+      <v-list>
+        <v-list-item>
+          <v-list-item-icon>
+            <v-icon>
+              mdi-home
+            </v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            Home
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item>
+          <v-list-item-icon>
+            <v-icon>
+              mdi-account-cog
+            </v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            Perfil
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-main>
+      <router-view></router-view>
+      Informação
+    </v-main>
+    <v-footer app class="py-3">
+      <span class="caption">
+        Fitme &copy; 2022
+      </span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      sidebar: true,
+      mini: false,
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container class="pa-8" fluid>
+    <h1 class="h1">
+      Fitme
+    </h1>
+  </v-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
